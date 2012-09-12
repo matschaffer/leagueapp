1549b46674c51a411da5ab5d330c8bbe16491c8f
Mat Schaffer <mat@schaffer.me>
2012-09-12 19:06:11 -0400

    Rails admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120912225612) do
+ActiveRecord::Schema.define(:version => 20120912230331) do
 
   create_table "games", :force => true do |t|
     t.datetime "starts_at"
@@ -38,6 +38,19 @@ ActiveRecord::Schema.define(:version => 20120912225612) do
   end
 
   add_index "players", ["team_id"], :name => "index_players_on_team_id"
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "teams", :force => true do |t|
     t.string   "name"
