91d3a89c4a36a900bbd4182895f44f118086af6d
Mat Schaffer <mat@schaffer.me>
2012-09-12 19:03:02 -0400

    Model fixes

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,7 @@
 class Team < ActiveRecord::Base
   attr_accessible :description, :logo, :name
   has_many :players
+  
+  has_many :home_games, class_name: 'Game', foreign_key: 'home_team_id'
+  has_many :away_games, class_name: 'Game', foreign_key: 'away_team_id'
 end
