5b08d45a02c1819bb3623682f57d7275d5173b2e
Mat Schaffer <mat@schaffer.me>
2012-09-12 21:18:10 -0400

    Cleaner game name fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,12 +3,16 @@ class Game < ActiveRecord::Base
   belongs_to :away_team, class_name: 'Team'
   belongs_to :venue
   attr_accessible :ends_at, :starts_at, :home_team_id, :away_team_id, :venue_id
-  
+
   validates :venue, presence: true
-  
+
   has_event_calendar :start_at_field  => 'starts_at', :end_at_field => 'ends_at'
-  
+
   def name
-    "#{home_team.name} vs #{away_team.name}"
+    if home_team && away_team
+      "#{home_team.name} vs #{away_team.name}"
+    else
+      "Teams TBD"
+    end
   end
 end
